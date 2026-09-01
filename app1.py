75185db605f17ce249e701d3e487ad96f36a530f
DIPk <deyou@example.com>
2026-09-01 21:19:25 +0530

    second commit

---
+++ app1.py
@@ -0,0 +1,5 @@
+paragraph = """Python is a high-level, interpreted programming language. 
+It is known for its clear syntax and readability. 
+Many beginners choose Python because it is easy to learn and very powerful."""
+
+print(paragraph)
